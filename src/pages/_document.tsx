4d02e046410b65bae62d33016562919414b13772
Dana Ciocan <danaciocan@economist.com>
2024-02-26 09:29:41 +0000

    Set up basic homepage and product page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <nav>this is a navigation menu</nav>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Product() {
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -13,7 +16,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>Home page</main>
+      <main className={`${styles.main} ${inter.className}`}>
+        Product page: {router.query.id}
+      </main>
     </>
   );
 }
